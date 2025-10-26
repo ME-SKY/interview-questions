b853120d04f3d022da5db957ec4d4a1211aed195
ME-SKY <meskalitofly@gmail.com>
2025-10-27 01:41:25 +1000

    add event-loop task example

---
+++ event-loop.js
@@ -0,0 +1,13 @@
+/* what gets logged? */
+
+Promise.resolve().then(() => console.log('1'));
+
+setTimeout(() => console.log('2'), 10);
+
+queueMicrotask(() => {
+  console.log('3');
+
+  queueMicrotask(() => console.log('4'));
+});
+
+console.log('5');
